a7dd582130a476238926866fb460045983d64cff
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-12-07 14:50:47 +0900

    for mfa

--- domain_search.py
+++ domain_search.py
@@ -5,20 +5,52 @@ from Bio import Seq
 
 def check_CENPB(inputf, inputfmt):
     CENPB = Seq.Seq("NTTCGNNNNANNCGGGN")
-    for seq_record in SeqIO.parse(inputf, inputfmt):
-        i = 0
-        while True:
-            seq = seq_record.seq[i:i+17]
-            if len(seq) < 17:
-                break
-            elif seq[1:5] == CENPB[1:5] and seq[9] == CENPB[9] and seq[12:16] == CENPB[12:16]:
-                yield [i, seq]
-                i += 1
-            else:
-                i += 1
+    rec_iter = SeqIO.parse(inputf, inputfmt)
+    while True:
+        try:
+            seq_record = next(rec_iter)
+        except StopIteration:
+            break
+        else:
+            i = 0
+            while True:
+                seq = seq_record.seq[i:i+17]
+                if len(seq) < 17:
+                    break
+                elif (seq[1:5] == CENPB[1:5] and
+                      seq[9] == CENPB[9] and seq[12:16] == CENPB[12:16]):
+                    yield [seq_record.id, i, seq]
+                    i += 1
+                else:
+                    i += 1
 
 
-search_CENPB = check_CENPB("DXZ1_HOR_2000.fa", "fasta")
+def check_CENPBrev(inputf, inputfmt):
+    CENPB = Seq.Seq("NTTCGNNNNANNCGGGN")
+    rec_iter = SeqIO.parse(inputf, inputfmt)
+    while True:
+        try:
+            seq_record = next(rec_iter)
+        except StopIteration:
+            break
+        else:
+            i = 0
+            while True:
+                seq = seq_record.seq[i:i+17]
+                seqr = seq[::-1]
+                if len(seq) < 17:
+                    break
+                elif (seqr[1:5] == CENPB[1:5] and
+                      seqr[9] == CENPB[9] and seqr[12:16] == CENPB[12:16]):
+                    yield [seq_record.id, i, seq]
+                    i += 1
+                else:
+                    i += 1
+
+
+print("forward")
+
+search_CENPB = check_CENPB("GenData2015_cons.fa", "fasta")
 while True:
     try:
         found = next(search_CENPB)
@@ -26,3 +58,27 @@ while True:
         break
     else:
         print(found)
+
+
+print("reverse")
+
+search_CENPBrev = check_CENPBrev("GenData2015_cons.fa", "fasta")
+while True:
+    try:
+        foundr = next(search_CENPBrev)
+    except StopIteration:
+        break
+    else:
+        print(foundr)
+
+
+ def testrev(seq1, seq2):
+    seqr = seq1[::-1]
+    if seqr[1:5] == seq2[1:5]:
+        return "True"
+
+
+seq1 = Seq.Seq("ATCGTTG")
+seq2 = Seq.Seq("ATTGCTA")
+
+print(testrev(seq1, seq2))
