ac44f1c531a042db59780bb84f84d9d4dd85e59e
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2017-12-11 15:04:15 +0900

    modified

--- domain_search.py
+++ domain_search.py
@@ -1,4 +1,3 @@
-from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from Bio import Seq
 
@@ -48,8 +47,54 @@ def check_CENPBrev(inputf, inputfmt):
                     i += 1
 
 
+def check_CENPBrevcom(inputf, inputfmt):
+    CENPB = Seq.Seq("NTTCGNNNNANNCGGGN")
+    rec_iter = SeqIO.parse(inputf, inputfmt)
+    while True:
+        try:
+            seq_record = next(rec_iter)
+        except StopIteration:
+            break
+        else:
+            i = 0
+            while True:
+                seq = seq_record.seq[i:i+17]
+                seqrc = seq.reverse_complement()
+                if len(seq) < 17:
+                    break
+                elif (seqrc[1:5] == CENPB[1:5] and
+                      seqrc[9] == CENPB[9] and seqrc[12:16] == CENPB[12:16]):
+                    yield [seq_record.id, i, seq]
+                    i += 1
+                else:
+                    i += 1
+
+
+def check_CENPBcom(inputf, inputfmt):
+    CENPB = Seq.Seq("NTTCGNNNNANNCGGGN")
+    rec_iter = SeqIO.parse(inputf, inputfmt)
+    while True:
+        try:
+            seq_record = next(rec_iter)
+        except StopIteration:
+            break
+        else:
+            i = 0
+            while True:
+                seq = seq_record.seq[i:i+17]
+                seqc = seq.complement()
+                if len(seq) < 17:
+                    break
+                elif (seqc[1:5] == CENPB[1:5] and
+                      seqc[9] == CENPB[9] and seqc[12:16] == CENPB[12:16]):
+                    yield [seq_record.id, i, seq]
+                    i += 1
+                else:
+                    i += 1
+
+
 def main():
-    inputf = "/Users/hamazaki/db/blast/GenData2015_cons.fa"
+    inputf = "DXZ_HOR_2000.fa"
     print("forward")
     search_CENPB = check_CENPB(inputf, "fasta")
     while True:
@@ -58,16 +103,25 @@ def main():
         except StopIteration:
             break
         else:
-            print(found)
-    print("reverse")
-    search_CENPBrev = check_CENPBrev(inputf, "fasta")
+            print(found[1] % 2054, found[2])
+    print("reverse_complement")
+    search_CENPBrevcon = check_CENPBrevcom(inputf, "fasta")
     while True:
         try:
-            foundr = next(search_CENPBrev)
+            foundrc = next(search_CENPBrevcon)
         except StopIteration:
             break
         else:
-            print(foundr)
+            print(foundrc)
+    print("complement")
+    search_CENPBcom = check_CENPBcom(inputf, "fasta")
+    while True:
+        try:
+            foundc = next(search_CENPBcom)
+        except StopIteration:
+            break
+        else:
+            print(foundc)
 
 
 if __name__ == '__main__':
